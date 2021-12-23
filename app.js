6f922d5c548210ec4ae114c0da4914adc595c9c8
alirbukhari <alirbukhar7@gmail.com>
2021-12-24 00:18:34 +0500

    ejs views

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', async (req, res) => {
+  res.render('pages/index', { title: 'yeShirts' });
+});
+
+app.get('/feedback', async (req, res) => {
+  res.render('pages/feedback', { title: 'yeShirts' });
+});
+
+app.get('/status', async (req, res) => {
   res.send({ success: true, message: 'Hello World' });
 });
 
