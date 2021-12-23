a76e0807d0e77b292d4b51739967600a095380d1
alirbukhari <alirbukhar7@gmail.com>
2021-12-24 00:38:56 +0500

    ejs server side

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// application routes
+app.get('/status', async (req, res) => {
+  return res.send({ success: true, message: 'Hello World' });
+});
+
+// server-side routes
 app.get('/', async (req, res) => {
-  res.render('pages/index', { title: 'yeShirts' });
+  return res.render('pages/index', { title: 'yeShirts' });
 });
 
 app.get('/feedback', async (req, res) => {
-  res.render('pages/feedback', { title: 'yeShirts' });
+  return res.render('pages/feedback', { title: 'yeShirts' });
 });
 
-app.get('/status', async (req, res) => {
-  res.send({ success: true, message: 'Hello World' });
+app.post('/feedback', async (req, res) => {
+  if (!Object.keys(req.body).length) return res.send({ success: false, message: 'fields missing !' });
+  return res.send({ success: true, message: 'Submitted!' });
 });
 
 app.listen(PORT, async () => {
