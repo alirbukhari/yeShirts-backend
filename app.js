9a0aa83ed55e1bf6d6e3f4c8b13ea98e1b2a09fc
alirbukhari <alirbukhar7@gmail.com>
2021-12-26 02:08:03 +0500

    user crud rest-api

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { nanoid } = require('nanoid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const users = {};
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -25,6 +28,39 @@ app.get('/feedback', async (req, res) => {
 app.post('/feedback', async (req, res) => {
   if (!Object.keys(req.body).length) return res.send({ success: false, message: 'fields missing !' });
   return res.send({ success: true, message: 'Submitted!' });
+});
+
+// USER CRUD Operations
+
+app.post('/user/login', async (req, res) => {
+  const user = Object.values(users).find((user) => user.email === req.body.email);
+
+  if (!user || user.password !== req.body.password) return res.status(403).json({ success: false, message: 'invalid credentials' });
+
+  return res.json(user);
+});
+
+app.post('/user', async (req, res) => {
+  const user = { id: nanoid(6), ...req.body };
+
+  users[user.id] = user;
+
+  return res.json({ success: true, message: 'User Registered' });
+});
+
+app.put('/user/:id', async (req, res) => {
+  const user = users[req.params.id];
+
+  if (!user) return res.status(401).json({ success: false, message: 'no user found' });
+
+  users[req.params.id] = { ...user, ...req.body };
+
+  return res.json({ success: true, message: 'user updated' });
+});
+
+app.delete('/user/:id', async (req, res) => {
+  delete users[req.params.id];
+  return res.json({ success: true, message: 'user deleted' });
 });
 
 app.listen(PORT, async () => {
